257e1687f28bf202ff264f4c0fdea95a92df0073
Your Name <okhuomonajayi54@gmail.com>
2023-12-04 13:01:38 +0100

    fix

--- app/agency/[taskId]/page.tsx
+++ app/agency/[taskId]/page.tsx
@@ -133,25 +133,25 @@ export default async function UserPage({ params: { taskId } }: Params) {
 
   return (
     <>
-      <div className="max-w-[1440px] mx-auto">
-        <h2 className="text-[60px] border-b-[1px] border-black">{task.name}</h2>
+      <div className="max-w-[1440px] mx-auto px-5 pb-10">
+        <h2 className="xl:text-[60px] text-[40px] border-b-[1px] border-black">{task.name}</h2>
         <div>
-          <div className="flex flex-row items-center gap-5 mt-10">
-            <div className="relative w-[150px] h-[100px]">
+          <div className="flex xl:flex-row flex-col xl:items-center gap-5 mt-10">
+            <div className="relative xl:w-[150px] w-[150px] h-[70px] ">
               <Image
                 src={task.logoURL}
                 layout="fill"
-                objectFit="cover"
+                objectFit="contain"
                 alt="logo"
               />
             </div>
 
-            <div className="flex gap-3 flex-col w-[80%]">
+            <div className="flex sm:gap-3 flex-col xl:w-[80%] -mt-6 xk:mt-0">
               <div className="flex gap-2">
                 <span className="font-bold">Business Name:</span>
                 <h5>{task.name}</h5>
               </div>
-              <div className="flex gap-2">
+              <div className="flex xl:flex-row flex-col gap-2">
                 <span className="font-bold">Services:</span>
                 <h5 className="text-blue-700">
                   <Link href="/">{task.services}</Link>
@@ -163,10 +163,10 @@ export default async function UserPage({ params: { taskId } }: Params) {
               </div>
             </div>
           </div>
-          <div className="flex flex-row items-center gap-5 mt-10">
-            <div className="relative w-[150px] h-[100px]"></div>
+          <div className="flex xl:flex-row flex-col items-center gap-5 xl:mt-10 mt-3">
+            <div className="relative w-[150px] h-[100px] sm:flex hidden"></div>
             <div className="mt-2 flex  gap-2 flex-col">
-              <div className="flex gap-2">
+              <div className="flex gap-2 sm:flex-row flex-col">
                 <span className="font-bold">Business Website:</span>
                 <h5 className="text-blue-700">
                   <Link href="/">{task.website}</Link>
