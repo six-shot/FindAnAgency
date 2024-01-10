bd93b561bcb97f8d28652b46ec84f131efae46b1
Your Name <okhuomonajayi54@gmail.com>
2024-01-10 09:48:05 +0100

    fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -50,15 +50,18 @@ function Hero() {
         <div className=""></div>
         <div className="  flex w-full h-full justify-center flex-col items-center ">
           <h1 className="font-silka font-bold xl:text-[56px] capitalize text-[26px] px-[5%]   text-center xl:leading-[56px] leading-[32px] z-50">
-            The most comprehensive listing of <br className="xl:flex hidden" /> marketing agencies in
-            Nigeria
+            The most comprehensive listing of <br className="xl:flex hidden" />{" "}
+            marketing agencies in Nigeria
           </h1>
-          <p className="text-white silka xl:w-[700px] xl:text-base text-sm px-[5%]  font-medium text-center xl:mt-10 mt-5 z-50">
+          <p className="text-white silka xl:w-[700px] xl:text-base text-sm px-[5%]  font-medium text-center xl:my-10 my-5  z-50">
             This comprehensive resource serves as a valuable directory for
             businesses seeking specialized marketing services, offering a broad
             spectrum of agencies with varying expertise, ensuring a one-stop
             reference for navigating the dynamic marketing landscape in Nigeria.
           </p>
+          <button className="text-xs py-3.5 px-8 font-silka font-medium text-white bg-[#192dad] rounded-[50px] z-50">
+            Find Agency
+          </button>
         </div>
       </div>
     </div>
