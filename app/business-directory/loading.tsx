c363eeb8a39982eb33e7b244ca764f9e239b3086
Your Name <okhuomonajayi54@gmail.com>
2024-01-16 11:18:54 +0100

    flex

--- app/business-directory/loading.tsx
+++ app/business-directory/loading.tsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function loading() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center font-nunito">
-      <h5 className="text-base">loading....</h5>
-      <h5 className="text-base">Hopefully not for too long....</h5>
+      <span className="loading"></span>
     </div>
   );
 }
